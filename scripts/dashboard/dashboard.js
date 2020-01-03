14b9d8617f770c747bd2c9083f482212e410cbd6
LaupWing <loc_strict@hotmail.com>
2020-01-03 20:35:09 +0100

    update

--- scripts/dashboard/dashboard.js
+++ scripts/dashboard/dashboard.js
@@ -1,10 +1,13 @@
 import Component from '../Component.js'
 import Profile from './profile/profile.js'
+import Habits from './habits/habits.js'
+
 class Dashboard extends Component{
     constructor(rootId){
-        super(rootId)
+        super(rootId, false, [{name:'id', value: 'dashboard'}])
         new Profile()
         this.create()
+        new Habits('dashboard')
     }
     create(){
         const container = document.createElement('div')
--- scripts/dashboard/habits/habits.js
+++ scripts/dashboard/habits/habits.js
@@ -1,7 +1,34 @@
-import Component from "../../Component";
+import Component from '../../Component.js';
+import db from '../../db/db.js'
 
 class Habits extends Component{
-    
+    constructor(rootId){
+        super(rootId)
+        this.create()
+        this.dataWatcher()
+    }
+    dataWatcher(){
+        db.collection('habits').onSnapshot(async (snapshot)=>{
+            snapshot.docChanges().forEach(change=>{
+                if(change.doc.id === firebase.auth().currentUser.uid){
+                    console.log('render via data')
+                }else{
+                    this.zeroState()
+                }
+            })
+        })
+    }
+    zeroState(){
+        this.element.innerHTML = `
+            <p>Add your first habit!</p>
+        `
+    }
+    create(){
+        const container = document.createElement('div')
+        container.className = 'habits'
+        this.element = container
+        this.attach()
+    }
 }
 
 export default Habits
